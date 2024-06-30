8bb4804480f9f3038c7f94aa2aff7ff7ffc5595c
NachoR9 <nachorm9@hotmail.com>
2024-06-29 21:54:35 -0700

    cherry appears after pacman eats three ghosts

--- pacman.js
+++ pacman.js
@@ -1,22 +1,31 @@
-const ghost = document.getElementById("ghost")
-const pacman = document.getElementById("pacman")
+const ghost = document.getElementById("ghost");
+const pacman = document.getElementById("pacman");
+let count = 0
 
 const eatGhost = (e) => {
-    const xposition = e.pageX - e.offsetX
-    const yposition = e.pageY - e.offsetY
-    pacman.style.position = "absolute"
-    pacman.style.left = xposition + "px"
-    pacman.style.top = yposition + "px"
-    // e.target.parentNode.removeChild(e.target)
-    e.target.style.visibility = "hidden"
-}
+  const xposition = e.pageX - e.offsetX;
+  const yposition = e.pageY - e.offsetY;
+  pacman.style.position = "absolute";
+  pacman.style.left = xposition + "px";
+  pacman.style.top = yposition + "px";
+  e.target.style.visibility = "hidden";
+  count++
+  if (count %3 === 0) {
+    const cherry = document.createElement("img")
+    cherry.src = "./images/cherry.png"
+    document.getElementById("pacman-container").append(cherry)
+    cherry.className = "cherry"
+  }
+};
 
-ghost.addEventListener("click", eatGhost)
+ghost.addEventListener("click", eatGhost);
 
- setInterval(function appears() {
-    const newGhost = document.createElement("img")
-    newGhost.src = "./images/fantasmito.png"
-    newGhost.className = "ghost"
-    newGhost.addEventListener("click", eatGhost)
-    document.getElementById("pacman-container").append(newGhost)
- }, 2000)
+setInterval(function appears() {
+  const newGhost = document.createElement("img");
+  newGhost.src = "./images/fantasmito.png";
+  newGhost.className = "ghost";
+  newGhost.addEventListener("click", eatGhost);
+  document.getElementById("pacman-container").append(newGhost);
+}, 2000);
+
+
