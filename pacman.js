83edf441c1b5ad83b09d389e54cd18afbdb110d6
NachoR9 <nachorm9@hotmail.com>
2024-06-29 21:29:10 -0700

    a ghost appears every 2 seconds and pacman can eat them

--- pacman.js
+++ pacman.js
@@ -1,12 +1,22 @@
 const ghost = document.getElementById("ghost")
 const pacman = document.getElementById("pacman")
 
-ghost.addEventListener("click", (e) => {
+const eatGhost = (e) => {
     const xposition = e.pageX - e.offsetX
     const yposition = e.pageY - e.offsetY
     pacman.style.position = "absolute"
     pacman.style.left = xposition + "px"
     pacman.style.top = yposition + "px"
-    ghost.parentNode.removeChild(e.target);
+    // e.target.parentNode.removeChild(e.target)
+    e.target.style.visibility = "hidden"
+}
 
-})
+ghost.addEventListener("click", eatGhost)
+
+ setInterval(function appears() {
+    const newGhost = document.createElement("img")
+    newGhost.src = "./images/fantasmito.png"
+    newGhost.className = "ghost"
+    newGhost.addEventListener("click", eatGhost)
+    document.getElementById("pacman-container").append(newGhost)
+ }, 2000)
